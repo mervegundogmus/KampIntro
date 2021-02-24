8adaf3a009f9a054ec15bf8595fe28e93f7a7fb3
mervegundogmus <mervegundogmus@outook.com>
2021-02-24 03:57:27 +0300

    23 Ocak

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Merve");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Ahmet");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "Merve", "Ahmet", "Mert", "Ali" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            isimler = new string[5];
+            isimler[4] = "Anıl";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+            List
+
+
+
+        }
+    }
+}
